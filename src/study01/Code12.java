10fa25e1fc6235ea824407473cb00bba2697c33b
kaaang <kaaang@kakao.com>
2022-04-20 23:14:08 +0900

    day 02

---
+++ src/study01/Code12.java
@@ -0,0 +1,32 @@
+package study01;
+
+import java.util.Scanner;
+
+public class Code12 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int [] data = new int [n];
+
+        for(int i=0;i<n;i++){
+            data[i] = kb.nextInt();
+        }
+        kb.close();
+
+        int maxSum = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum = 0;
+                for(int k=i;k<=j;k++){
+                    sum += data[k];
+                }
+                if(sum > maxSum){
+                    maxSum = sum;
+                }
+            }
+        }
+
+        System.out.println("The max sum is " + maxSum);
+
+    }
+}
