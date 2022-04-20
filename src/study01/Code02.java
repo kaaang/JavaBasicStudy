32c7a5057229b9aa910bb9be6245e3c73052dd36
kaaang <kaaang@kakao.com>
2022-04-20 20:01:41 +0900

    day 02

--- src/study01/Code02.java
+++ src/study01/Code02.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class Code02 {
     public static void main(String[] args) {
         int number = 123;
-        Scanner keyboard = new Scanner(System.in);
+        Scanner kb = new Scanner(System.in);
         System.out.println("Please enter an integer : ");
-        int input = keyboard.nextInt();
+        int input = kb.nextInt();
 
         if(input == number){
             System.out.println("Numbers match! :-)");
         }else{
             System.out.println("Numbers do not match! :-(");
         }
+
+        kb.close();
     }
 }
---
+++ src/study01/Code03.java
@@ -0,0 +1,24 @@
+package study01;
+
+import java.util.Scanner;
+
+public class Code03 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String input = null;
+
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please type a string : ");
+
+        input = kb.next();
+
+//        if(str == input){
+        if(str.equals(input)){
+            System.out.println("Strings match! :-)");
+        }else{
+            System.out.println("String do not match! :-(");
+        }
+
+        kb.close();
+    }
+}
