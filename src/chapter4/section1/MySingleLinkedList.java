6fdedd8bf633ae1f03e96d1569282b08576bc5ca
kaaang <kaaang@kakao.com>
2022-06-05 00:30:53 +0900

    day 11

---
+++ src/chapter4/section1/MySingleLinkedList.java
@@ -0,0 +1,13 @@
+package chapter4.section1;
+
+public class MySingleLinkedList<T> {
+
+    public Node<T> head;
+    public int size = 0;
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
